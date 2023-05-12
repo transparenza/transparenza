d6eea4c6c9f8c42d2082d5f55ef8fd50f168f172
ferrodri <f.rodriguez.hervias@gmail.com>
2023-05-12 23:58:04 +0100

    add state

--- contracts/src/Review.sol
+++ contracts/src/Review.sol
@@ -6,8 +6,16 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 import {IERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
 import {IERC165} from "forge-std/interfaces/IERC165.sol";
+import "forge-std/console.sol";
 
 contract Review {
+    /// Token => EOA => hasCommented
+    mapping(address => mapping(address => bool)) public accountReviewedToken;
+    /// Token => id => EOA => hasCommented
+    mapping(address => mapping(uint256 => mapping(address => bool))) public accountReviewed1155;
+    /// EOA => counter
+    mapping(address => uint256) public counter;
+
     bytes32 public immutable name = "Transparenza";
     bytes4 private immutable _interfaceIdERC1155 = 0xd9b67a26;
     bytes4 private immutable _interfaceIdERC721 = 0x80ac58cd;
@@ -15,6 +23,10 @@ contract Review {
     function commentERC20(address token, string calldata cid, string calldata pOPId) public {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.balanceOf.selector, msg.sender));
         _checkIsHolder(success, data);
+
+        _setReview(token, msg.sender);
+
+        _count(msg.sender);
     }
 
     function commentERC721(address token, string calldata cid, string calldata pOPId) public {
@@ -25,6 +37,10 @@ contract Review {
 
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC721.balanceOf.selector, msg.sender));
         _checkIsHolder(success, data);
+
+        _setReview(token, msg.sender);
+
+        _count(msg.sender);
     }
 
     function commentERC1155(address token, uint256 id, string calldata cid, string calldata pOPId) public {
@@ -36,6 +52,24 @@ contract Review {
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(IERC1155.balanceOf.selector, msg.sender, id));
         _checkIsHolder(success, data);
+
+        _setReview1155(token, id, msg.sender);
+
+        _count(msg.sender);
+    }
+
+    function _count(address sender) private {
+        counter[sender] = counter[sender] + 1;
+    }
+
+    function _setReview(address token, address sender) private {
+        require(!accountReviewedToken[token][sender], "Already commented");
+        accountReviewedToken[token][sender] = true;
+    }
+
+    function _setReview1155(address token, uint256 id, address sender) private {
+        require(!accountReviewed1155[token][id][sender], "Already commented");
+        accountReviewed1155[token][id][sender] = true;
     }
 
     function _checkIsHolder(bool success, bytes memory data) private pure {
--- contracts/test/Review.t.sol
+++ contracts/test/Review.t.sol
@@ -31,16 +31,38 @@ contract ReviewTest is Test {
     function test_commentERC20() public {
         vm.prank(alice);
         review.commentERC20(address(mockERC20), "cid", "pOPId");
+        assertEq(review.accountReviewedToken(address(mockERC20), alice), true);
+        assertEq(review.counter(alice), 1);
     }
 
     function test_commentERC721() public {
         vm.prank(alice);
         review.commentERC721(address(mockERC721), "cid", "pOPId");
+        assertEq(review.accountReviewedToken(address(mockERC721), alice), true);
+        assertEq(review.counter(alice), 1);
     }
 
     function test_commentERC1155() public {
         vm.prank(alice);
         review.commentERC1155(address(mockERC1155), 0, "cid", "pOPId");
+        assertEq(review.accountReviewed1155(address(mockERC1155), 0, alice), true);
+        assertEq(review.counter(alice), 1);
+    }
+
+    function test_RevertWhen_DoubleOpinionToken() public {
+        vm.startPrank(alice);
+        review.commentERC20(address(mockERC20), "cid", "pOPId");
+        vm.expectRevert("Already commented");
+        review.commentERC20(address(mockERC20), "cid", "pOPId");
+        vm.stopPrank();
+    }
+
+    function test_RevertWhen_DoubleOpinion1155() public {
+        vm.startPrank(alice);
+        review.commentERC1155(address(mockERC1155), 0, "cid", "pOPId");
+        vm.expectRevert("Already commented");
+        review.commentERC1155(address(mockERC1155), 0, "cid", "pOPId");
+        vm.stopPrank();
     }
 
     function test_RevertWhen_NotHolder() public {
