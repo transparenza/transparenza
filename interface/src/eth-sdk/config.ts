7dfec391efc0dbd132a4cdbc814477c20bbf2b9f
ferrodri <f.rodriguez.hervias@gmail.com>
2023-05-13 17:14:42 +0100

    add lisbon txt

--- interface/src/eth-sdk/config.ts
+++ interface/src/eth-sdk/config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     //   Transparenza: '0xD81dE4BCEf43840a2883e5730d014630eA6b7c4A',
     // },
     polygonMumbai: {
-      Transparenza: '0x3af10c9c65F627b71dF7003022528b35E049Aee4'
+      Transparenza: '0x5df2bb9020b3e6aa3d6543376477ac29d3ee5312'
     }
   }
 })
